26523b715ee743900efbc8c652c1024bfb245584
Usakant <168073653+Usakant@users.noreply.github.com>
2026-05-30 22:53:46 +0530

    Add PersonRepository interface extending JpaRepository

---
+++ src/main/java/com/personalinfo/repository/PersonRepository.java
@@ -0,0 +1,12 @@
+package com.personalinfo.repository;
+
+import com.personalinfo.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    Optional<Person> findByEmail(String email);
+}
