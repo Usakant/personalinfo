21c3f6abe332e09cd04fae709314f3a920bb76df
Usakant <umajhee.2012@gmail.com>
2026-06-04 11:42:17 +0530

    implement swagger

--- src/main/java/com/personalinfo/PersonalInfoApplication.java
+++ src/main/java/com/personalinfo/PersonalInfoApplication.java
@@ -1,9 +1,18 @@
 package com.personalinfo;
-
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Personal Information API",
+				version = "1.0",
+				description = "API documentation for managing Personal Details"
+		)
+)
 @SpringBootApplication
+
 public class PersonalInfoApplication {
 
     public static void main(String[] args) {
